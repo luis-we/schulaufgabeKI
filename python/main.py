a495cbb90be11fbc479322d386257429516b423d
DanTheMan <danTheMan@yourmama.com>
2023-05-08 09:24:46 +0200

    python

--- python/main.py
+++ python/main.py
@@ -1,6 +1,10 @@
-import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 
 app = FastAPI()
 
@@ -19,6 +23,16 @@ app.add_middleware(
 
 @app.post("/")
 async def create_files(files: list[UploadFile] = File(...)):
-    print("test")
-         
+    class_names = []
+    images_original = []
+    images_small = []
+    
+    for file in files:
+        label = file.filename.split("_")[0]
+
+        if label not in class_names:
+            class_names.append(label)
+        
+       
     return {"filenames": [file.filename for file in files]}
+    
